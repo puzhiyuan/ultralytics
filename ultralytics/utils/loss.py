7797988de645ab94828e48ec2fe5f7ebeb3686c7
puzhiyuan <puzhiyuan185489643@gmail.com>
2024-04-10 14:49:00 +0800

    CIoU -> CIoU_Focaler

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -73,7 +73,8 @@ class BboxLoss(nn.Module):
         """IoU loss."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
         # TODO modify IOU step1
-        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
+        # iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
+        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, FIoU=True)
         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
         # DFL loss
