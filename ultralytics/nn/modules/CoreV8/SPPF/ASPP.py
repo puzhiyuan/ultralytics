acc5e041e1a02a80a8b923c523c2c89e9729aff9
‘puzhiyuan’ <puzhiyuan185489643@gmail.com>
2024-04-11 21:54:47 +0800

    change upsample to interpolate

--- ultralytics/nn/modules/CoreV8/SPPF/ASPP.py
+++ ultralytics/nn/modules/CoreV8/SPPF/ASPP.py
@@ -21,7 +21,7 @@ class ASPP(nn.Module):
 
         image_features = self.mean(x)
         image_features = self.conv(image_features)
-        image_features = F.upsample(image_features, size=size, mode='bilinear')
+        image_features = F.interpolate(image_features, size=size, mode='bilinear')
 
         atrous_block1 = self.atrous_block1(x)
         atrous_block6 = self.atrous_block6(x)
